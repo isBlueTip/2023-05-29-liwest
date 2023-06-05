5be00d9dbbbb846dfff4c5cc34c1e5b0f6b9adbf
Simon Egorov <rhinoRofl@gmail.com>
2023-06-05 16:00:34 +0600

    Create views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from api.serializers import ProductCategorySerializer, ProductGroupSerializer, ProductSerializer
+from products.models import Product, ProductCategory, ProductGroup
+
+
+class ProductCategoryListCreateView(generics.ListCreateAPIView):
+    queryset = ProductCategory.objects.all()
+    serializer_class = ProductCategorySerializer
+
+
+class ProductGroupListCreateView(generics.ListCreateAPIView):
+    queryset = ProductGroup.objects.all()
+    serializer_class = ProductGroupSerializer
+
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductGroupDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ProductGroup.objects.all()
+    serializer_class = ProductGroupSerializer
